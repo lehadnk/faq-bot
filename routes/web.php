5565f98216463b916462024ffe3aedc4b8c92e4d
Алексей Заузин <lehadnk@Aleksejs-MacBook-Pro.local>
2022-02-23 21:39:16 +0300

    Added how-to-use page

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/how-to-use', function () {
+    return view('how-to-use');
 });
